41fc583502a83a3fa2c39f50952274d29def0265
Tierra <50035689+RuckerTierra@users.noreply.github.com>
2021-06-11 22:45:39 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+
+// Goal: Working blackjack Gamepa
+// Step 1: 
+
+// elements
+let messageEl = document.getElementById("message");
+let cardsEl = document.getElementById("cards");
+let sumEl = document.getElementById("sum");
+
+// 
