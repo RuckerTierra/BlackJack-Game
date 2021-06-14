373cc17fe022dbcc43291c9d70f4f5e90c53f7eb
Tierra <50035689+RuckerTierra@users.noreply.github.com>
2021-06-14 17:36:58 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,49 @@
 
-// Goal: Working blackjack Gamepa
-// Step 1: 
+// Goal: Working blackjack Game
+// Step 1: display message, cards & sum
+// Step 2: new game 
+// Step 3: new card 
+
 
 // elements
-let messageEl = document.getElementById("message");
-let cardsEl = document.getElementById("cards");
-let sumEl = document.getElementById("sum");
+let firstCard = 10;
+let secondCard = 4;
+let cards = [firstCard, secondCard];
+let sum = firstCard + secondCard;
+let isAlive = true;
+let hasBlackjack = false;
+let message = "";
+let messageEl = document.getElementById("message-el");
+let cardsEl = document.getElementById("cards-el");
+let newCardEl = document.getElementById("newCard-el");
+let sumEl = document.getElementById("sum-el");
 
-// 
+ 
+// messageEl.textContent = "";
+cardsEl.textContent = "Cards: " + firstCard + ", " + secondCard;
+sumEl.textContent = "Sum: " + sum;
+
+function startGame(){
+  renderGame()
+}
+
+function renderGame(){
+  if (sum <= 19){
+    message = "Do you want to draw a new card?"
+  } else if (sum === 21) {
+    message = "Congrats! You got Blackjack!";
+    hasBlackjack = true;
+  } else {
+    message = "Game over!";
+    isAlive = false;
+  }
+  messageEl.textContent = message;
+}
+
+
+function newCard(){
+  let card = 5;
+  sum += card;
+  newCardEl.textContent = "New Card: " + card;
+  sumEl.textContent = "Sum: " + sum;
+}
